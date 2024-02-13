719d4cafb6e048a5bf9cbd3c079edbbb6bdc07fd
Aisha <159571882+Aisha-216@users.noreply.github.com>
2024-02-13 21:46:36 +0500

    Add files via upload

---
+++ Q 1/main.ts
@@ -0,0 +1,6 @@
+// Question # 1:
+// Install Node.js, TypeScript and VS Code on your computer.
+
+// 1. Install [Node.js](https://nodejs.org/en/), [TypeScript](https://www.typescripting.org/download) and [Vs Code](https://code.visualstudio.com) on your computer.
+
+
